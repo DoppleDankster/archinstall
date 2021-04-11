5f67038b7cdacffa4ab1caf2454e27a4497781bd
Doppledankster <davidmedioni@protonmail.com>
2021-04-12 01:41:47 +0200

    Add SSH support

--- home_install.sh
+++ home_install.sh
@@ -3,15 +3,21 @@
 # Install OhMyZsh
 cd "$HOME"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-rm "$HOME/.zshrc"
+git clone https://github.com/zsh-users/zsh-autosuggestions.git "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
+
+# SSH
+mkdir -p $HOME/.ssh
+ssh-keygen -b 4096 -t rsa -C "github" -f $HOME/.ssh/github
+ssh-keygen -b 4096 -t rsa -C "github" -f $HOME/.ssh/gitlab
+ssh-keygen -b 4096 -t rsa -C "github" -f $HOME/.ssh/server
 
 # Dootfiles
 mkdir -p "$HOME/Git" && cd "$HOME/Git"
 mkdir -p "$HOME/.doom.d/"
 git clone https://github.com/DoppleDankster/dootfiles.git
 find "$HOME/Git/dootfiles" -type f -iname "*.sh" -exec chmod +x {} \;
+rm "$HOME/.zshrc"
 stow -v -R -t ~ dootfiles
 . /home/doppledankster/.zshrc
 
@@ -23,6 +29,5 @@ git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
 
 # Python packages
 pip install -r /archinstall/packages/python.txt
-
 
 
