b580545496b760041522ff5276206fcafd538327
Doppledankster <davidmedioni@protonmail.com>
2021-04-11 16:20:36 +0200

    Add Home Dotfile script

---
+++ home_install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Install OhMyZsh
+cd "$HOME"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+rm "$HOME/.zshrc"
+
+# Dootfiles
+mkdir -p "$HOME/Git" && cd "$HOME/Git"
+mkdir -p "$HOME/.doom.d/"
+git clone https://github.com/DoppleDankster/dootfiles.git
+find "$HOME/Git/dootfiles" -type f -iname "*.sh" -exec chmod +x {} \;
+stow -v -R -t ~ dootfiles
+. /home/doppledankster/.zshrc
+
+
+# Install Doom Emacs
+git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
+~/.emacs.d/bin/doom -y install
+~/.emacs.d/bin/doom -y sync
+
+# Python packages
+pip install -r /archinstall/python.txt
+
+
+
---
+++ yay_install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cd $HOME
+git clone https://aur.archlinux.org/yay-git.git
+cd yay-git
+makepkg -si 
+cd $HOME
+echo "Cleaning Up Yay install folder"
+rm -rf $HOME/yay-git
+echo "Installing Yay packages"
+yay -Syu -q --noconfirm
+yay -S -q --noconfirm - < /archinstall/yay.txt
