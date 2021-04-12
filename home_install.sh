cef16df4fbebdd168effc89d5223ceab10e34cad
Doppledankster <davidmedioni@protonmail.com>
2021-04-12 12:36:49 +0200

    fixe home install

--- home_install.sh
+++ home_install.sh
@@ -13,11 +13,19 @@ ssh-keygen -b 4096 -t rsa -C "github" -f $HOME/.ssh/gitlab
 ssh-keygen -b 4096 -t rsa -C "github" -f $HOME/.ssh/server
 
 # Dootfiles
+
+# cloning the dependencies
 mkdir -p "$HOME/Git" && cd "$HOME/Git"
 mkdir -p "$HOME/.doom.d/"
 git clone https://github.com/DoppleDankster/dootfiles.git
 find "$HOME/Git/dootfiles" -type f -iname "*.sh" -exec chmod +x {} \;
+
+# seting up everything
 rm "$HOME/.zshrc"
+mkdir -p $HOME/.config
+mkdir -p $HOME/.local
+
+# applying dotfiles
 stow -v -R -t ~ dootfiles
 . /home/doppledankster/.zshrc
 
